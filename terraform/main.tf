7c1873f193fbb547ca667cef788718fb0936bbac
David Munguia <david.munguia@viaro.net>
2024-10-01 00:41:30 -0600

    add aws provider to tf script

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,21 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "digitalocean" {
   token = var.do_token
+}
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_access_key
+  region     = var.aws_region
 }
 
 variable "do_token" {
