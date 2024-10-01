23147e28d57a5ef232af6783c37e7cfdf05d06f4
David Munguia <david.munguia@viaro.net>
2024-10-01 03:00:01 -0600

    dasdasd

--- seedDb.py
+++ seedDb.py
@@ -5,7 +5,7 @@ dynamodb = boto3.resource('dynamodb', region_name='us-eas-1')
 table = dynamodb.Table('Metaphoto_test')
 
 # Load the data from the JSON file
-with open('./dynamo_data.json') as f:
+with open('/home/root/app/dynamo_data.json') as f:
     data = json.load(f)
 
 # Insert each item into the new DynamoDB table
